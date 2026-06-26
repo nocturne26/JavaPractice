d4cd002c0c467192119e710cc3c57e27bd643685
nocturne26 <cfpope2016@gmail.com>
2026-06-26 01:45:38 -0600

    add and move files to Intro

---
+++ Intro/FollowAlong2.java
@@ -0,0 +1,13 @@
+//Chris Pope
+// CIS 125 - Intro to Programming
+
+public class FollowAlong2 {
+    public static void main(String[] args) {
+        int num = 13;
+        float afloat = 43.6666666667f;
+
+        System.out.println("Num: " + num);
+        System.err.println("Float: " + afloat);
+
+    }
+}
