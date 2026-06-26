d20e396fc74a89804935626a585590055d57295e
nocturne26 <cfpope2016@gmail.com>
2026-06-26 01:05:34 -0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+//Chris Pope
+// CIS 125 - Intro to Programming
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am here.");
+        System.out.println("No More Coral");
+    }
+}
